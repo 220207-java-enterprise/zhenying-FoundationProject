e4d7836e252b89b829e3d5206de09b66779e2d6c
zhenying <a664330348@gmail.com>
2022-03-01 14:15:50 -0500

    update dtos

--- src/main/java/com/revature/foundationproject/dtos/responses/ErsUserResponse.java
+++ src/main/java/com/revature/foundationproject/dtos/responses/ErsUserResponse.java
@@ -1,2 +1,49 @@
-package com.revature.foundationproject.dtos.responses;public class ErsUserResponse {
+package com.revature.foundationproject.dtos.responses;
+
+import com.revature.foundationproject.models.ErsUser;
+
+public class ErsUserResponse {
+    private String user_id;
+    private String username;
+    private String given_name;
+    private String role;
+
+    public ErsUserResponse(ErsUser user) {
+        this.user_id = user.getUser_id();
+        this.username = user.getUsername();
+        this.given_name = user.getGiven_name();
+        this.role = user.getRole().getRole_name();
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGiven_name() {
+        return given_name;
+    }
+
+    public void setGiven_name(String given_name) {
+        this.given_name = given_name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- src/main/java/com/revature/foundationproject/dtos/responses/Principal.java
+++ src/main/java/com/revature/foundationproject/dtos/responses/Principal.java
@@ -1,2 +1,52 @@
-package com.revature.foundationproject.dtos.responses;public class Principal {
+package com.revature.foundationproject.dtos.responses;
+
+import com.revature.foundationproject.models.ErsUser;
+
+public class Principal {
+    private String user_id;
+    private String username;
+    private String role;
+
+    public Principal() {
+        super();
+    }
+
+    public Principal(ErsUser user) {
+        this.user_id = user.getUser_id();
+        this.username = user.getUsername();
+        this.role = user.getRole().getRole_name();
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Principal{" +
+                "user_id='" + user_id + '\'' +
+                ", username='" + username + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
