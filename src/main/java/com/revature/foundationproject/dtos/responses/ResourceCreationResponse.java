dd12bbb62261c6d2cae55154a66ce7eb121a429d
zhenying <a664330348@gmail.com>
2022-03-01 18:48:03 -0500

    update dtos

--- src/main/java/com/revature/foundationproject/dtos/responses/ResourceCreationResponse.java
+++ src/main/java/com/revature/foundationproject/dtos/responses/ResourceCreationResponse.java
@@ -1,2 +1,18 @@
-package com.revature.foundationproject.dtos.responses;public class ResourceCreationResponse {
+package com.revature.foundationproject.dtos.responses;
+
+public class ResourceCreationResponse {
+    private String id;
+
+    public ResourceCreationResponse(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
 }
